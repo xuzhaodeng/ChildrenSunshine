39761e0ddd9e3f88e76f0970bf69662aac232e05
eric <ericli2wang@gmail.com>
2017-06-01 14:57:05 +0800

    多环境配置。文档生成开关配置

--- src/main/java/com/pas/edu/config/SwaggerConfig.java
+++ src/main/java/com/pas/edu/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.pas.edu.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -21,12 +22,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Value("${swagger.show}")
+    private boolean swaggerShow;
     /**
      * 可以定义多个组，比如本类中定义把test和demo区分开了 （访问页面就可以看到效果了）
      */
     @Bean
     public Docket testApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .enable(this.swaggerShow)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.pas.edu.api"))
