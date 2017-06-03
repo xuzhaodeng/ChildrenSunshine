9004e3a5b484c9f1172b89a474baee0be91ff580
xuzhao_deng <xuzhao_deng@sina.com>
2017-06-03 14:33:09 +0800

    数据采集、数据更新、数据详细、村管端列表

---
+++ src/main/java/com/pas/edu/api/ChildApplyController.java
@@ -0,0 +1,52 @@
+package com.pas.edu.api;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pas.edu.entity.ChildRoster;
+import com.pas.edu.entity.common.Result;
+import com.pas.edu.service.ChildApplyService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * Author : 邓子
+ * CreateDate : 2017/6/2 
+ * Version : 2.0
+ * Desc :  数据采集接口
+ * Modified :
+ */
+@Api(value = "花名册", tags = "花名册接口")
+@RestController
+@RequestMapping("api/roster")
+public class ChildApplyController {
+	
+	@Resource
+	private ChildApplyService caService;
+	
+	@ApiOperation(value = "花名册", notes = "花名册添加")
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public Result addRoster(@RequestBody ChildRoster childRoster) throws Exception {
+        Result result = new Result();
+        caService.addChildRoster(childRoster);
+        return result;
+    }
+	
+	
+	@ApiOperation(value = "花名详情", notes = "获取花名册信息")
+	@ApiImplicitParam(name = "id", paramType = "path", value = "机构id", required = true, dataType = "int")
+	@RequestMapping(value = "detail/{id}", method = RequestMethod.GET)
+	public Result getRosterDetailed(@PathVariable int id) throws Exception {
+        Result result = new Result();
+        //result.setData(organ);
+        return result;
+    }
+
+}
---
+++ src/main/java/com/pas/edu/service/ChildApplyService.java
@@ -0,0 +1,8 @@
+package com.pas.edu.service;
+
+import com.pas.edu.entity.ChildRoster;
+
+public interface ChildApplyService {
+	
+	void addChildRoster(ChildRoster childRoster); 
+}
