9c1e92c2a29ae79fefa0d87ad847eb7146ac21b9
xuzhao_deng <xuzhao_deng@sina.com>
2017-06-03 20:18:15 +0800

    --

---
+++ src/main/java/com/pas/edu/entity/RosterRequest.java
@@ -0,0 +1,14 @@
+package com.pas.edu.entity;
+
+import lombok.Data;
+
+@Data
+public class RosterRequest {
+	
+	private Integer uid;
+	
+	private Integer currPage;
+	
+	private Integer pageSize;
+
+}
