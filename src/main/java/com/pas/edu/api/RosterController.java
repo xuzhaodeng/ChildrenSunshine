5badfe0dbbd797fffb7f763f61b7ed2111f7f706
xuzhao_deng <xuzhao_deng@sina.com>
2017-06-05 16:28:12 +0800

    --

---
+++ src/main/java/com/pas/edu/api/RosterController.java
@@ -0,0 +1,35 @@
+package com.pas.edu.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pas.edu.entity.common.Result;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * Author : 邓子
+ * CreateDate : 2017/6/5 
+ * Version : 2.0
+ * Desc :  花名册查询列表
+ * Modified :
+ */
+@Api(value = "花名册查询列表", tags = "花名册查询列表")
+@RestController
+@RequestMapping("api/roster")
+public class RosterController extends BaseController {
+	
+	@ApiOperation(value = "获取村管端困境儿童列表", notes = "获取村管端困境儿童列表")
+	@RequestMapping(value = "getlsts", method = RequestMethod.GET)
+	public Result getRosterLsts(@RequestParam(value = "uid", required = true) Integer uid,
+			//@RequestParam(value = "uid", required = true) Integer uid,
+			@RequestParam(value = "currPage", required = true) Integer currPage, 
+			@RequestParam(value = "pageSize", required = true) Integer pageSize) throws Exception{
+		Result result = new Result();
+		return result;
+	}
+
+}
