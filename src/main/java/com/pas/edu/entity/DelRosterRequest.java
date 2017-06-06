cde13e2689a4ea77e3930de4fd46b132a83bd0d3
xuzhao_deng <xuzhao_deng@sina.com>
2017-06-06 19:33:10 +0800

    --接口对应的注释

--- src/main/java/com/pas/edu/entity/DelRosterRequest.java
+++ src/main/java/com/pas/edu/entity/DelRosterRequest.java
@@ -1,12 +1,15 @@
 package com.pas.edu.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class DelRosterRequest {
 	
+	@ApiModelProperty("操作用户ID")
 	private Integer uid;
 	
+	@ApiModelProperty("儿童ID，多个Id使用逗号分隔")
 	private String childIds;
 
 }
