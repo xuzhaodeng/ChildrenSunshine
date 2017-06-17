e4cc9f973eb856bb6f239033a63557042d42488d
THINK <THINK@163.com>
2017-06-17 10:13:38 +0800

    添加评估保障实体类级映射

---
+++ src/main/java/com/pas/edu/dao/SafeguardRecordDao.java
@@ -0,0 +1,7 @@
+package com.pas.edu.dao;
+
+/**
+ * Created by THINK on 2017/6/16.
+ */
+public class SafeguardRecordDao {
+}
---
+++ src/main/java/com/pas/edu/entity/SafeguardRecord.java
@@ -0,0 +1,7 @@
+package com.pas.edu.entity;
+
+/**
+ * Created by THINK on 2017/6/16.
+ */
+public class SafeguardRecord {
+}
