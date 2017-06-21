4c09baa912ec5afe95f733d04f6c15afaf9811d3
THINK <THINK@163.com>
2017-06-21 16:48:00 +0800

    添加评估统计报表

--- src/main/java/com/pas/edu/service/SafeguardRecordService.java
+++ src/main/java/com/pas/edu/service/SafeguardRecordService.java
@@ -2,6 +2,7 @@ package com.pas.edu.service;
 
 
 import com.pas.edu.entity.SafeguardRecord;
+import com.pas.edu.entity.SafeguardReport;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface SafeguardRecordService {
     public void createSafeguardRecord(SafeguardRecord safeguardRecord);
     public void updateSafeguardRecord(SafeguardRecord safeguardRecord);
     public SafeguardRecord getSafeguardRecord(int safeguardId);
-    public List<SafeguardRecord> findSafeguardRecordList(Integer orgId);
+    public List<SafeguardRecord> findSafeguardRecordList(Integer orgId, String beginTime, String endTime);
+    public List<SafeguardReport> getSafeguardReport(int orgId, String beginTime, String endTime);
 }
