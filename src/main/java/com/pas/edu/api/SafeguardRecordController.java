793efc75248c040252788393fa47835eb0ea4f34
shihuo <zhang-sh@qq.com>
2017-06-30 23:42:56 +0800

    修改以下字段类别为空的问题：基本生活保障		教育保障		基本医疗保障		落实监护责任		残疾儿童福利服务

--- src/main/java/com/pas/edu/api/SafeguardRecordController.java
+++ src/main/java/com/pas/edu/api/SafeguardRecordController.java
@@ -1,10 +1,12 @@
 package com.pas.edu.api;
 
+import com.pas.edu.common.DictionaryHelper;
 import com.pas.edu.entity.ChildRoster;
 import com.pas.edu.entity.SafeguardRecord;
 import com.pas.edu.entity.SafeguardReport;
 import com.pas.edu.entity.common.BaseResult;
 import com.pas.edu.service.ChildApplyService;
+import com.pas.edu.service.DatadictService;
 import com.pas.edu.service.SafeguardRecordService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Api(value = "保障评估接口", tags = "保障评估接口")
 @RestController
@@ -25,6 +28,8 @@ public class SafeguardRecordController extends BaseController{
     private SafeguardRecordService safeguardRecordService;
     @Autowired
     private ChildApplyService childApplyService;
+    @Autowired
+    private DatadictService datadictService;
 
     @ApiOperation(value = "保障评估统计", notes = "保障评估统计")
     @ApiImplicitParams( value = {
@@ -45,9 +50,17 @@ public class SafeguardRecordController extends BaseController{
     @ApiImplicitParam(paramType = "query", name = "orgId", value = "机构id", required = true, dataType = "int")
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public BaseResult<List<SafeguardRecord>> list(@RequestParam(value = "orgId", required = true) Integer orgId, @RequestParam(value = "beginTime", required = false) String beginTime, @RequestParam(value = "endTime", required = false) String endTime) {
+        Map<String,String> jhqkMap = datadictService.getDatadictMap(DictionaryHelper.DATA_TYPE_JHQK);
+        Map<String,String> kjlbMap = datadictService.getDatadictMap(DictionaryHelper.DATA_TYPE_KJLB);
+        Map<String,String> jbshqkMap = datadictService.getDatadictMap(DictionaryHelper.DATA_TYPE_JBSHQK);
+        Map<String,String> jyqkMap = datadictService.getDatadictMap(DictionaryHelper.DATA_TYPE_JYQK);
+        Map<String,String> ylqkMap = datadictService.getDatadictMap(DictionaryHelper.DATA_TYPE_YLQK);
+        Map<String,String> flqkMap = datadictService.getDatadictMap(DictionaryHelper.DATA_TYPE_FLQK);
+
         List<SafeguardRecord> safeguardRecordList = safeguardRecordService.findSafeguardRecordList(orgId, beginTime, endTime);
         for(SafeguardRecord safeguardRecord:safeguardRecordList) {
             ChildRoster childRoster = childApplyService.getRosterInfoByChildId(safeguardRecord.getChildId());
+            childApplyService.decorateChildRoster(childRoster,jhqkMap,kjlbMap,jbshqkMap,jyqkMap,ylqkMap,flqkMap);
             safeguardRecord.setChildRoster(childRoster);
         }
         BaseResult result = new BaseResult();
